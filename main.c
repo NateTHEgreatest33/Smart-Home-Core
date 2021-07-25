c4bb45cb97e750a652307796a878815440c4cab6
Nate Lenze <nlenze@umich.edu>
2021-07-24 19:43:40 -0500

    pull in bug fixes and pull up submodules

--- main.c
+++ main.c
@@ -16,13 +16,17 @@
 #include <stdbool.h>
 #include <string.h>
 #include <stdio.h>
-
-#include "messageAPI/messageAPI.h"
+#include "messageAPI/LoRa/LoraAPI.h"
+//#include "messageAPI/messageAPI.h"
 #include "init.h"
+
 
 /*--------------------------------------------------------------------
                           LITERAL CONSTANTS
 --------------------------------------------------------------------*/
+#define RED_LED               ( 0x02 )      /* value for red LED    */
+#define GREEN_LED             ( 0x08 )      /* value for Green LED  */
+#define BLUE_LED              ( 0x04 )      /* value for Blue LED   */
 
 /*--------------------------------------------------------------------
                                 TYPES
@@ -31,7 +35,6 @@
 /*--------------------------------------------------------------------
                            MEMORY CONSTANTS
 --------------------------------------------------------------------*/
-static const location current_module = MODULE_NONE;
 
 /*--------------------------------------------------------------------
                               VARIABLES
@@ -70,11 +73,10 @@ IntMasterDisable();
 
 /* interrupt handler code here */
 
-GPIOIntClear( INTERRUPT_PORT, INTERRUPT_PIN );
+//GPIOIntClear( INTERRUPT_PORT, INTERRUPT_PIN );
 IntMasterEnable();
 
 }
-
 
 /*********************************************************************
 *
@@ -94,15 +96,25 @@ int main
 Local variables
 ----------------------------------------------------------*/
 lora_errors lora_err_var;
-rx_message rx_msg;
-tx_message tx_msg;
+lora_config lora_config_port;
+uint8_t msgArr[4];
+uint8_t returnMsgArr[10];
+uint8_t size;
+uint8_t returnSize;
 
+msgArr[0] = 0x00;
+msgArr[1] = 0x11;
+msgArr[2] = 0x22;
+msgArr[3] = 0x33;
+size = 4;
 /*----------------------------------------------------------
 Initialize local variables
 ----------------------------------------------------------*/
 lora_err_var = RX_NO_ERROR;
-memset( &rx_msg, 0, sizeof( rx_message ) );
-memset( &tx_msg, 0, sizeof( tx_message ) );
+
+
+lora_config_port.SSI_BASE = 0x40008000;
+lora_config_port.SSI_PORT = PORT_A;
 
 /*----------------------------------------------------------
 Initialize all subsystems while interrupts are disabled
@@ -110,51 +122,40 @@ Initialize all subsystems while interrupts are disabled
 IntMasterDisable();
 
 sub_system_init();
-lora_err_var = init_message();
+lora_port_init(lora_config_port);
+lora_init_tx();
+
+
 
 IntMasterEnable();
 
-/*----------------------------------------------------------
-If issue with subsystem initilization do not move forward
-----------------------------------------------------------*/
-if ( lora_err_var != RX_NO_ERROR )
-    {
-    GPIO_PORTF_DATA_R = 0x2;                                 //red
-    while( true )
-        {  
-        }
-    }
 
 /*----------------------------------------------------------
 Set LED to signify start of main process loop
 ----------------------------------------------------------*/
-GPIO_PORTF_DATA_R = 0x08;                                   //green
+GPIO_PORTF_DATA_R = GREEN_LED; 
 
 /*----------------------------------------------------------
-Main processing loop
+TEST procedure
 ----------------------------------------------------------*/
+
+lora_send_message( msgArr, size );
+
+if( lora_init_continious_rx() != true)
+{
+	while(true)
+		{
+		}
+}
+size = 10;
 while( true )
     {
-    /*----------------------------------------------------------
-    Check for new messages
-    ----------------------------------------------------------*/
-    if( get_message( &rx_msg, &lora_err_var ) )
-        {
-        /*----------------------------------------------------------
-        Proccess new messages if sent to us
-        ----------------------------------------------------------*/
-        if( rx_msg.destination == current_module )
-            {
-            /*----------------------------------------------------------
-            Send Reply
-            ----------------------------------------------------------*/
-            send_message(tx_msg);
-
-            }
-
-        }
-
-    }
+    if( lora_get_message( returnMsgArr, size, &returnSize, &lora_err_var) )
+			{
+				while( true)
+				{}
+			}
+	}
 
 } /* main() */
 

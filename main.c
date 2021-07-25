4a04925ed9ba6876925919e6a427d010aede4316
Nate <nlenze@umich.edu>
2021-07-24 21:18:56 -0500

    add tools + make updates for moduleAPI

--- main.c
+++ main.c
@@ -16,8 +16,7 @@
 #include <stdbool.h>
 #include <string.h>
 #include <stdio.h>
-#include "messageAPI/LoRa/LoraAPI.h"
-//#include "messageAPI/messageAPI.h"
+#include "messageAPI/messageAPI.h"
 #include "init.h"
 
 
@@ -35,6 +34,7 @@
 /*--------------------------------------------------------------------
                            MEMORY CONSTANTS
 --------------------------------------------------------------------*/
+static const location current_module = TIVA_MODULE;
 
 /*--------------------------------------------------------------------
                               VARIABLES
@@ -95,24 +95,18 @@ int main
 /*----------------------------------------------------------
 Local variables
 ----------------------------------------------------------*/
+rx_message rx_msg;
+tx_message tx_msg;
 lora_errors lora_err_var;
 lora_config lora_config_port;
-uint8_t msgArr[4];
-uint8_t returnMsgArr[10];
-uint8_t size;
-uint8_t returnSize;
 
-msgArr[0] = 0x00;
-msgArr[1] = 0x11;
-msgArr[2] = 0x22;
-msgArr[3] = 0x33;
-size = 4;
 /*----------------------------------------------------------
 Initialize local variables
 ----------------------------------------------------------*/
+memset( &rx_msg, 0, sizeof( rx_message ) );
+memset( &tx_msg, 0, sizeof( tx_message ) );
+
 lora_err_var = RX_NO_ERROR;
-
-
 lora_config_port.SSI_BASE = 0x40008000;
 lora_config_port.SSI_PORT = PORT_A;
 lora_config_port.SSI_PIN = 0x08;
@@ -123,13 +117,20 @@ Initialize all subsystems while interrupts are disabled
 IntMasterDisable();
 
 sub_system_init();
-lora_port_init(lora_config_port);
-lora_init_tx();
-
-
+lora_err_var = init_message( lora_config_port );
 
 IntMasterEnable();
 
+/*----------------------------------------------------------
+If issue with subsystem initilization do not move forward
+----------------------------------------------------------*/
+if ( lora_err_var != RX_NO_ERROR )
+    {
+    GPIO_PORTF_DATA_R = RED_LED;
+    while( true )
+        {  
+        }
+    }
 
 /*----------------------------------------------------------
 Set LED to signify start of main process loop
@@ -139,23 +140,35 @@ GPIO_PORTF_DATA_R = GREEN_LED;
 /*----------------------------------------------------------
 TEST procedure
 ----------------------------------------------------------*/
+tx_msg.destination = RPI_MODULE;
+tx_msg.source = current_module;
+tx_msg.message[0] = 0x00;
+tx_msg.message[1] = 0x11;
+tx_msg.message[2] = 0x22;
+tx_msg.message[3] = 0x33;
+tx_msg.size = 4;
 
-lora_send_message( msgArr, size );
-
-if( lora_init_continious_rx() != true)
-{
-	while(true)
-		{
-		}
-}
-size = 10;
+		
 while( true )
     {
-    if( lora_get_message( returnMsgArr, size, &returnSize, &lora_err_var) )
-			{
-				while( true)
-				{}
-			}
+		/*----------------------------------------------------------
+    Check for new messages
+    ----------------------------------------------------------*/
+    if( get_message( &rx_msg, &lora_err_var ) )
+        {
+        /*----------------------------------------------------------
+        Proccess new messages if sent to us
+        ----------------------------------------------------------*/
+        if( rx_msg.destination == current_module )
+            {
+            /*----------------------------------------------------------
+            Send Reply
+            ----------------------------------------------------------*/
+            send_message(tx_msg);
+
+            }
+
+        }
 	}
 
 } /* main() */

074880f655e26513d6c3561466b1b62f0df13b77
Nate Lenze <nlenze@umich.edu>
2023-11-08 19:44:46 -0500

    fix system test test cases for pi pico

--- vnv/system/pi_pico_test.py
+++ vnv/system/pi_pico_test.py
@@ -23,21 +23,22 @@ testIndex = ( 'test_description', 'send_data', 'destination', 'expected_data' )
 dTestIndex = ( 'test_description', 'raw_send', 'expected_data')
 
 
+
 #---------------------------------------------------------------------
 #                      TEST CASE VARIABLES
 #---------------------------------------------------------------------
-#					  test_description		  	    send_data													    dest  expected data
+#Test cases
+#					  test_description		  		 send_data													  dest  expected data
 test_case_group = [ ( "Test 1 byte message",  		[0xFF],															0x02, [0xAA, 0x11] 	),
 					( "Test 10 byte message", 		[0x01, 	0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A],  0x02, [0xAA, 0x22 ] ),
 					( "Send 0 byte message",  		[], 															0x02, [0xBB, 0x03] 	),
 					( "send to unsupported module", [0xFF],															0x05, []			) ]
 
 #								("description",    			[dest, src,  pad, ver/size, key, DATA... crc], (True, source, data, valid) )
-distructive_test_case_group = [ ("Test CRC Error", 			[0x00, 0x01, 0x00, 0x11, 0x00, 0xFF, 0xFF], 	(True, 0x01, [0xBB, 0x01], True) ),
-								("Test Size (Big) Error", 	[0x00, 0x01, 0x00, 0x1F, 0x00, 0xFF, 0xFF], 	(True, 0x01, [0xBB, 0x02], True) ),
-								("Test Size (Small) Error", [0x00, 0x01, 0x00, 0x10, 0x00, 0xFF], 			(True, 0x01, [0xBB, 0x03], True) ),
-								("Test Key Error", 			[0x00, 0x01, 0x00, 0x11, 0xFF, 0xFF, 0x97], 	(True, 0x01, [0xBB, 0x04], True) ) ]
-
+distructive_test_case_group = [ ("Test CRC Error", 			[0x02, 0x01, 0x00, 0x11, 0x00, 0xFF, 0xFF], 	(True, 0x02, [0xBB, 0x01], True) ),
+								("Test Size (Big) Error", 	[0x02, 0x01, 0x00, 0x1F, 0x00, 0xFF, 0xFF], 	(True, 0x02, [0xBB, 0x02], True) ),
+								("Test Size (Small) Error", [0x02, 0x01, 0x00, 0x10, 0x00, 0xFF], 			(True, 0x02, [0xBB, 0x03], True) ),
+								("Test Key Error", 			[0x02, 0x01, 0x00, 0x11, 0xFF, 0xFF, 0xF9 ], 	(True, 0x02, [0xBB, 0x04], True) ) ]
 
 #---------------------------------------------------------------------
 #                             CLASSES

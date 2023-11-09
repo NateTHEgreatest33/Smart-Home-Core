561b833b712da9e81c478bc121aabd32315926c0
Nate Lenze <nlenze@umich.edu>
2023-11-09 13:47:23 -0500

    squash all personal branch changes into one commit for pi pico

--- main.cpp
+++ main.cpp
@@ -16,10 +16,11 @@
 #include "pico/stdlib.h"
 #include "pico/cyw43_arch.h"
 #include "hardware/spi.h"
+#include "pico/error.h"
 
 extern "C" 
     {
-    #include "LoraAPI.h"
+    #include "messageAPI.h"
     }
 
 /*--------------------------------------------------------------------
@@ -33,10 +34,10 @@ extern "C"
                                 TYPES
 --------------------------------------------------------------------*/
 
-// /*--------------------------------------------------------------------
-//                            MEMORY CONSTANTS
-// --------------------------------------------------------------------*/
-// const location current_location = PICO_MODULE;
+/*--------------------------------------------------------------------
+                           MEMORY CONSTANTS
+--------------------------------------------------------------------*/
+const location current_location = PICO_MODULE;
 
 /*--------------------------------------------------------------------
                               VARIABLES
@@ -69,198 +70,121 @@ int main
 /*----------------------------------------------------------
 Local variables
 ----------------------------------------------------------*/
-// rx_message rx_msg;
-// tx_message tx_msg;
-// lora_errors lora_err_var;
-// lora_config lora_config_port;
+rx_message       rx_msg;
+tx_message       tx_msg;
+lora_errors      lora_err_var;
+bool             sdio_err_var;
+pico_error_codes wifi_err_var;
+bool             msgRxed;
 
 /*----------------------------------------------------------
 Initialize local variables
 ----------------------------------------------------------*/
-// memset( &rx_msg, 0, sizeof( rx_message ) );
-// memset( &tx_msg, 0, sizeof( tx_message ) );
+memset( &rx_msg, 0, sizeof( rx_message ) );
+memset( &tx_msg, 0, sizeof( tx_message ) );
 
-// lora_err_var = RX_NO_ERROR;
-// lora_config_port.SSI_BASE = 0x40008000;
-// lora_config_port.SSI_PORT = PORT_A;
-// lora_config_port.SSI_PIN = 0x08;
+lora_err_var = RX_NO_ERROR;
+sdio_err_var = false;
+wifi_err_var = PICO_ERROR_GENERIC;
 
 /*----------------------------------------------------------
-Initialize all subsystems while interrupts are disabled
+Initialize all subsystems   
 ----------------------------------------------------------*/
-stdio_init_all();
+sdio_err_var = stdio_init_all();
+wifi_err_var = (pico_error_codes) cyw43_arch_init();
+lora_err_var = init_message( spi_default );
 
-if( cyw43_arch_init() )
+/*----------------------------------------------------------
+If issue with subsystem initilization do not move forward
+----------------------------------------------------------*/
+if ( lora_err_var != RX_NO_ERROR || wifi_err_var != PICO_OK || !sdio_err_var )
     {
-    printf("Wi-Fi init failed");
-    return -1;
+
+    while( true )
+        {  
+        }
     }
-    
-lora_port_init( spi_default );
-lora_init_continious_rx();
 
-// /*----------------------------------------------------------
-// If issue with subsystem initilization do not move forward
-// ----------------------------------------------------------*/
-// if ( lora_err_var != RX_NO_ERROR )
-//     {
-
-//     while( true )
-//         {  
-//         }
-//     }
-
-// /*----------------------------------------------------------
-// Set LED on to signify start of main process loop
-// ----------------------------------------------------------*/
+/*----------------------------------------------------------
+Set LED on to signify start of main process loop
+----------------------------------------------------------*/
 cyw43_arch_gpio_put( CYW43_WL_GPIO_LED_PIN, 1 );
 
+/*----------------------------------------------------------
+System Test procedure/replies
+----------------------------------------------------------*/
+while( true )
+    {
+	/*----------------------------------------------------------
+    Check for new messages
+    ----------------------------------------------------------*/
+    msgRxed = get_message( &rx_msg, &lora_err_var );
+
+    /*----------------------------------------------------------
+    Non distructive testing replies
+    ----------------------------------------------------------*/
+    if(  msgRxed == true && lora_err_var == RX_NO_ERROR )
+        {
+        memset( &tx_msg, 0, sizeof( tx_message ) );
+        tx_msg.destination = RPI_MODULE;
+
+        tx_msg.message[0] = 0xAA;
+        tx_msg.size       = 2;
+        switch( rx_msg.size )
+            {
+            case 0x01:
+                tx_msg.message[1] = 0x11;
+                break;
+            case 0x0A:
+                tx_msg.message[1] = 0x22;
+                break;
+            default:
+                tx_msg.message[1] = 0xFF;
+                break;
+            }
+        send_message(tx_msg);
+        }
+
+	/*----------------------------------------------------------
+    Distructive testing
+    ----------------------------------------------------------*/
+    if(lora_err_var != RX_NO_ERROR )
+        {
+        memset( &tx_msg, 0, sizeof( tx_message ) );
+        tx_msg.destination = RPI_MODULE;
+
+        tx_msg.message[0] = 0xBB;
+        tx_msg.size       = 2;
+        switch( lora_err_var )
+            {
+            case RX_CRC_ERROR:
+                tx_msg.message[1] = 0x01;
+                break;
+            case RX_INVALID_HEADER:
+                tx_msg.message[1] = 0x02;
+                break;
+            case RX_SIZING:
+                tx_msg.message[1] = 0x03;
+                break;
+            case RX_KEY_ERR:
+                tx_msg.message[1] = 0x04;
+                break;
+            default:
+                tx_msg.message[1] = 0xFF;
+                break;
+            }
+        send_message(tx_msg);
+
+        }
 
 
+    /*----------------------------------------------------------
+    Clear errors and rx message for next round
+    ----------------------------------------------------------*/
+    memset( &rx_msg, 0, sizeof( rx_msg ) );
+    lora_err_var = RX_NO_ERROR;
+    msgRxed = false;
 
-
-
-
-
-
-
-
-
-
-
-
-
-/* --------------
-WORKING BUT TESTING
--------------- */
-
-
-
-// // #define PIN_CS   17
-// #define PIN_CS   28
-// #define ACTIVE 0
-// #define INACTIVE 1
-
-// spi_init(spi_default, 100000 );
-// spi_set_format(spi_default, 8, SPI_CPOL_0, SPI_CPHA_1, SPI_MSB_FIRST);
-// gpio_set_function(PICO_DEFAULT_SPI_RX_PIN, GPIO_FUNC_SPI);
-// gpio_set_function(PICO_DEFAULT_SPI_SCK_PIN, GPIO_FUNC_SPI);
-// gpio_set_function(PICO_DEFAULT_SPI_TX_PIN, GPIO_FUNC_SPI);
-// gpio_set_function(PICO_DEFAULT_SPI_CSN_PIN, GPIO_FUNC_SPI );
-// gpio_init(PIN_CS);
-// gpio_set_dir(PIN_CS, GPIO_OUT);
-// gpio_put(PIN_CS, INACTIVE );
-
-
-// while(1)
-//     {
-//     int i;
-//     sleep_ms( 1000 );
-
-//     uint8_t buffer[4]      = { 0x81, 0x80, 0x00, 0x00 }; //configure into sleep mode (0x81 -write reg 1 0x80, sleep mode)
-//     uint8_t rtn_buffer[4]  = { 0xFF, 0xFF, 0xFF, 0xFF }; //giberish?
-//     uint8_t buffer2[4]     = { 0x01, 0x00, 0x00, 0x00 }; // ( 0x01 - read from register 1)
-//     uint8_t rtn_buffer2[4] = { 0xFF, 0xFF, 0xFF, 0xFF }; // this should be the value 0x00 0x80
-
-
-
-//     gpio_put(PIN_CS, ACTIVE);  // Active low
-//     // spi_write_blocking(spi_default, buffer, 4 );
-//     // spi_read_blocking(spi_default, 0, rtn_buffer, 4);
-//     spi_write_read_blocking(spi_default, buffer, rtn_buffer, 2 );
-//     gpio_put(PIN_CS, INACTIVE);  // Active high
-
-    
-//     // sleep_ms(1);
-//     sleep_us( 10 ); //THIS IS REQUIRED TO ALLOW TIME FOR CS TO BE STOPPED!
-//     // i++;
-//     //this works, however the key is the 1ms sleep (could probably be less tbh)
-
-//     // spi_write_blocking(spi_default, buffer2, 2 );
-//     gpio_put(PIN_CS, ACTIVE);  // Active high
-//     spi_write_read_blocking(spi_default, buffer2, rtn_buffer2, 2 );
-
-//     gpio_put(PIN_CS, INACTIVE);  // Active high
-
-
-//     printf(" result here %d", i );
-//     }
-
-
-
-//     msg = [0x80 | 0x01, 0x80]
-//     result = spi.xfer2(msg)
-
-//     #high power tx mode
-//    # print("writing 0xFF to reg high power TX (0x09)")
-//     msg = [0x80 | 0x09,0xFF]
-//     result = spi.xfer2(msg)
-// /*----------------------------------------------------------
-// System Test procedure/replies
-// ----------------------------------------------------------*/
-// while( true )
-//     {
-// 	/*----------------------------------------------------------
-//     Check for new messages
-//     ----------------------------------------------------------*/
-//     if( get_message( &rx_msg, &lora_err_var ) )
-//         {
-//         /*----------------------------------------------------------
-//         Distructive testing replies
-//         ----------------------------------------------------------*/ 
-//         if( lora_err_var != RX_NO_ERROR )
-//             {
-//             memset( &tx_msg, 0, sizeof( tx_message ) );
-//             tx_msg.destination = RPI_MODULE;
-
-//             tx_msg.message[0] = 0xBB;
-//             tx_msg.size       = 2;
-//             switch( lora_err_var )
-//                 {
-//                 case RX_CRC_ERROR:
-//                     tx_msg.message[1] = 0x01;
-//                     break;
-//                 case RX_INVALID_HEADER:
-//                     tx_msg.message[1] = 0x02;
-//                     break;
-//                 case RX_SIZING:
-//                     tx_msg.message[1] = 0x03;
-//                     break;
-//                 case RX_KEY_ERR:
-//                     tx_msg.message[1] = 0x04;
-//                     break;
-//                 default:
-//                     tx_msg.message[1] = 0xFF;
-//                     break;
-//                 }
-//             send_message(tx_msg);
-
-//             }
-//         /*----------------------------------------------------------
-//         Non distructive testing replies
-//         ----------------------------------------------------------*/
-//         else
-//             {
-//             memset( &tx_msg, 0, sizeof( tx_message ) );
-//             tx_msg.destination = RPI_MODULE;
-
-//             tx_msg.message[0] = 0xAA;
-//             tx_msg.size       = 2;
-//             switch( rx_msg.size )
-//                 {
-//                 case 0x01:
-//                     tx_msg.message[1] = 0x11;
-//                     break;
-//                 case 0x0A:
-//                     tx_msg.message[1] = 0x22;
-//                     break;
-//                 default:
-//                     tx_msg.message[1] = 0xFF;
-//                     break;
-//                 }
-//             send_message(tx_msg);
-//             }
-//         }
-// 	}
+	}
 
 } /* main() */
---
+++ vnv/system/pi_pico_test.py
@@ -0,0 +1,142 @@
+#*********************************************************************
+#
+#   MODULE NAME:
+#       pi_pico_test.py - Pi Pico Test Script
+#
+#   DESCRIPTION:
+#       Prove message API functionality on the pi pico
+#
+#   Copyright 2023 by Nate Lenze
+#*********************************************************************
+
+#---------------------------------------------------------------------
+#                              IMPORTS
+#---------------------------------------------------------------------
+from msgAPI import messageAPI
+from enum import Enum
+import time
+
+#---------------------------------------------------------------------
+#                            VARIABLES
+#---------------------------------------------------------------------
+testIndex = ( 'test_description', 'send_data', 'destination', 'expected_data' )
+dTestIndex = ( 'test_description', 'raw_send', 'expected_data')
+
+#---------------------------------------------------------------------
+#                      TEST CASE VARIABLES
+#---------------------------------------------------------------------
+#Test cases
+#					  test_description		  		 send_data													  dest  expected data
+test_case_group = [ ( "Test 1 byte message",  		[0xFF],															0x02, [0xAA, 0x11] 	),
+					( "Test 10 byte message", 		[0x01, 	0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A],  0x02, [0xAA, 0x22 ] ),
+					( "Send 0 byte message",  		[], 															0x02, [0xBB, 0x03] 	),
+					( "send to unsupported module", [0xFF],															0x05, []			) ]
+
+#								("description",    			[dest, src,  pad, ver/size, key, DATA... crc],  (True, source, data, valid) )
+distructive_test_case_group = [ ("Test CRC Error", 			[0x02, 0x01, 0x00, 0x11, 0x00, 0xFF, 0xFF], 	(True, 0x02, [0xBB, 0x01], True) ),
+								("Test Size (Big) Error", 	[0x02, 0x01, 0x00, 0x1F, 0x00, 0xFF, 0xFF], 	(True, 0x02, [0xBB, 0x02], True) ),
+								("Test Size (Small) Error", [0x02, 0x01, 0x00, 0x10, 0x00, 0xFF], 			(True, 0x02, [0xBB, 0x03], True) ),
+								("Test Key Error", 			[0x02, 0x01, 0x00, 0x11, 0xFF, 0xFF, 0xF9 ], 	(True, 0x02, [0xBB, 0x04], True) ) ]
+
+#---------------------------------------------------------------------
+#                             CLASSES
+#---------------------------------------------------------------------
+
+# ==========================================================
+# Terminal Colors
+# ==========================================================
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+# ==========================================================
+# Test
+# ==========================================================
+class Test:
+    # ==================================
+    # Constructor
+    # ==================================
+	def __init__(self, bus, chip_select, currentModule, listOfModules, testCases, dis_testCases, module_under_test):
+		#setup local var's
+		self.module = messageAPI( bus=bus, chip_select=chip_select, currentModule=currentModule, listOfModules=listOfModules )
+		self.module.InitAPI()
+		self.TestCases = testCases
+		self.DistructiveTestCases = dis_testCases
+		self.module.debug_prints = False
+		self.module_under_test = module_under_test
+
+    # ==================================
+    # RunTestCases()
+    # ==================================
+	def RunTestCases(self):
+		for i in self.TestCases:
+			print( bcolors.ENDC + i[ testIndex.index('test_description') ] + ":", end = '')
+
+			self.module.TXMessage( message=i[testIndex.index('send_data')], destination=i[testIndex.index('destination')] )
+			time.sleep(2)
+			return_tpl = self.module.RXMessage()
+
+			if i[testIndex.index('expected_data')] == []:
+				if return_tpl == ( False, 0xFF, [], True ):
+					print(bcolors.OKGREEN + " PASSED")
+				else:
+					print(bcolors.FAIL + " FAILED")
+					print(bcolors.WARNING + "Expected: ( False, 0xFF, [], True ).\nReturned: "+ str(return_tpl) )
+
+			else:
+				if return_tpl == (True, i[testIndex.index('destination')], i[testIndex.index('expected_data')], True):
+					print(bcolors.OKGREEN + " PASSED")
+				else:
+					print(bcolors.FAIL + " FAILED")
+					print(bcolors.WARNING + "Expected: ( True, " + str(i[testIndex.index('destination')]) + ", " + str(i[testIndex.index('expected_data')]) + ", True )\nReturned: " + str(return_tpl) )
+
+
+    # ==================================
+    # RunDistructiveTesting()
+    #
+    # NOTE: This function is used to 
+    # test cases that cannot be properly
+    # done through the offical messageAPI
+    # ==================================
+	def RunDistructiveTesting(self):
+		for testcase in self.DistructiveTestCases:
+			#send raw data on LoRa
+
+			print( bcolors.ENDC + testcase[ dTestIndex.index('test_description') ] + ":", end = '')
+			self.module._messageAPI__LoraSendMessage(messageList=testcase[ dTestIndex.index('raw_send') ], messageSize=len( testcase[ dTestIndex.index('raw_send') ] ) )
+			self.module._messageAPI__LoraSetRxMode()
+			time.sleep(1)
+
+			#verify return data
+			return_tpl = self.module.RXMessage()
+			if testcase[ dTestIndex.index('expected_data') ] == return_tpl:
+				print(bcolors.OKGREEN + " PASSED")
+			else:
+				print(bcolors.FAIL + " FAILED")
+				print(bcolors.WARNING + "Expected: "+ str(testcase[ dTestIndex.index('expected_data') ]) +"\nReturned: "+ str(return_tpl) )
+
+
+
+#---------------------------------------------------------------------
+#                      MAIN FUNCTION
+#---------------------------------------------------------------------
+def main():
+    Test = Test( bus=0, chip_select=0, currentModule=0x00, listOfModules=[0x00,0x01,0x02], testCases=test_case_group, dis_testCases = distructive_test_case_group, module_under_test = 0x02 )
+    print(bcolors.HEADER + "Normal Test Cases:")
+    Test.RunTestCases()
+    print(bcolors.HEADER + "Distructive Test Cases:")
+    Test.RunDistructiveTesting()
+
+#---------------------------------------------------------------------
+#                      MAIN TEST LOOP
+#---------------------------------------------------------------------
+if __name__ == "__main__":
+  main()
+

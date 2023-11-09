02d9f2936489c977bd7281140c2adda5de82c2f9
Nate Lenze <nlenze@umich.edu>
2023-11-08 19:42:41 -0500

    fix main.cpp format to allow for returns that stuck through due to tiva being module 0x00

--- main.cpp
+++ main.cpp
@@ -75,6 +75,7 @@ tx_message       tx_msg;
 lora_errors      lora_err_var;
 bool             sdio_err_var;
 pico_error_codes wifi_err_var;
+bool             msgRxed;
 
 /*----------------------------------------------------------
 Initialize local variables
@@ -117,64 +118,73 @@ while( true )
 	/*----------------------------------------------------------
     Check for new messages
     ----------------------------------------------------------*/
-    if( get_message( &rx_msg, &lora_err_var ) )
+    msgRxed = get_message( &rx_msg, &lora_err_var );
+
+    /*----------------------------------------------------------
+    Non distructive testing replies
+    ----------------------------------------------------------*/
+    if(  msgRxed == true && lora_err_var == RX_NO_ERROR )
         {
-        /*----------------------------------------------------------
-        Distructive testing replies
-        ----------------------------------------------------------*/ 
-        if( lora_err_var != RX_NO_ERROR )
+        memset( &tx_msg, 0, sizeof( tx_message ) );
+        tx_msg.destination = RPI_MODULE;
+
+        tx_msg.message[0] = 0xAA;
+        tx_msg.size       = 2;
+        switch( rx_msg.size )
             {
-            memset( &tx_msg, 0, sizeof( tx_message ) );
-            tx_msg.destination = RPI_MODULE;
-
-            tx_msg.message[0] = 0xBB;
-            tx_msg.size       = 2;
-            switch( lora_err_var )
-                {
-                case RX_CRC_ERROR:
-                    tx_msg.message[1] = 0x01;
-                    break;
-                case RX_INVALID_HEADER:
-                    tx_msg.message[1] = 0x02;
-                    break;
-                case RX_SIZING:
-                    tx_msg.message[1] = 0x03;
-                    break;
-                case RX_KEY_ERR:
-                    tx_msg.message[1] = 0x04;
-                    break;
-                default:
-                    tx_msg.message[1] = 0xFF;
-                    break;
-                }
-            send_message(tx_msg);
-
+            case 0x01:
+                tx_msg.message[1] = 0x11;
+                break;
+            case 0x0A:
+                tx_msg.message[1] = 0x22;
+                break;
+            default:
+                tx_msg.message[1] = 0xFF;
+                break;
             }
-        /*----------------------------------------------------------
-        Non distructive testing replies
-        ----------------------------------------------------------*/
-        else
-            {
-            memset( &tx_msg, 0, sizeof( tx_message ) );
-            tx_msg.destination = RPI_MODULE;
-
-            tx_msg.message[0] = 0xAA;
-            tx_msg.size       = 2;
-            switch( rx_msg.size )
-                {
-                case 0x01:
-                    tx_msg.message[1] = 0x11;
-                    break;
-                case 0x0A:
-                    tx_msg.message[1] = 0x22;
-                    break;
-                default:
-                    tx_msg.message[1] = 0xFF;
-                    break;
-                }
-            send_message(tx_msg);
-            }
+        send_message(tx_msg);
         }
+
+	/*----------------------------------------------------------
+    Distructive testing
+    ----------------------------------------------------------*/
+    if(lora_err_var != RX_NO_ERROR )
+        {
+        memset( &tx_msg, 0, sizeof( tx_message ) );
+        tx_msg.destination = RPI_MODULE;
+
+        tx_msg.message[0] = 0xBB;
+        tx_msg.size       = 2;
+        switch( lora_err_var )
+            {
+            case RX_CRC_ERROR:
+                tx_msg.message[1] = 0x01;
+                break;
+            case RX_INVALID_HEADER:
+                tx_msg.message[1] = 0x02;
+                break;
+            case RX_SIZING:
+                tx_msg.message[1] = 0x03;
+                break;
+            case RX_KEY_ERR:
+                tx_msg.message[1] = 0x04;
+                break;
+            default:
+                tx_msg.message[1] = 0xFF;
+                break;
+            }
+        send_message(tx_msg);
+
+        }
+
+
+    /*----------------------------------------------------------
+    Clear errors and rx message for next round
+    ----------------------------------------------------------*/
+    memset( &rx_msg, 0, sizeof( rx_msg ) );
+    lora_err_var = RX_NO_ERROR;
+    msgRxed = false;
+
 	}
 
 } /* main() */
